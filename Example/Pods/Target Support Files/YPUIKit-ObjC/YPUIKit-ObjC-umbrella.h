9e7b571152848428e329f9d6da8e84030439f4e2
chenghengsheng <chenghs@shinemo.com>
2022-08-06 22:49:20 +0800

    增加NSDate一些扩展; 1.0.5 -> 1.0.6;
    Signed-off-by: chenghengsheng <chenghs@shinemo.com>

--- Example/Pods/Target Support Files/YPUIKit-ObjC/YPUIKit-ObjC-umbrella.h
+++ Example/Pods/Target Support Files/YPUIKit-ObjC/YPUIKit-ObjC-umbrella.h
@@ -10,6 +10,24 @@
 #endif
 #endif
 
+#import "YPAlertView.h"
+#import "YPButton.h"
+#import "NSData+YPExtension.h"
+#import "NSDate+YPExtension.h"
+#import "NSString+YPExtension.h"
+#import "NSTimer+YPExtension.h"
+#import "UIButton+YPExtension.h"
+#import "UIColor+YPExtension.h"
+#import "UIImage+YPExtension.h"
+#import "UILabel+YPExtension.h"
+#import "UIScrollView+YPExtension.h"
+#import "UITableViewCell+YPExtension.h"
+#import "UITextField+YPExtension.h"
+#import "UIView+YPExtension.h"
+#import "UIViewController+YPExtension.h"
+#import "YPCategoryHeader.h"
+#import "YPKitDefines.h"
+#import "YPUIKit.h"
 
 FOUNDATION_EXPORT double YPUIKit_ObjCVersionNumber;
 FOUNDATION_EXPORT const unsigned char YPUIKit_ObjCVersionString[];
---
+++ YPUIKit-ObjC/Classes/YPCategory/NSTimer+YPExtension.m
@@ -0,0 +1,12 @@
+//
+//  NSTimer+YPExtension.m
+//  YPUIKit-ObjC
+//
+//  Created by Hansen on 8/6/22.
+//
+
+#import "NSTimer+YPExtension.h"
+
+@implementation NSTimer (YPExtension)
+
+@end
