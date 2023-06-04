1d7da210f0a9a28c70902704fce8e8ef7a6909c9
chenghengsheng <chenghs@shinemo.com>
2023-06-04 18:41:31 +0800

    fix: 优化弹框逻辑
    Signed-off-by: chenghengsheng <chenghs@shinemo.com>

---
+++ YPUIKit-ObjC/Classes/YPPickerAlert/YPPickerAlert.h
@@ -0,0 +1,16 @@
+//
+//  YPPickerAlert.h
+//  YPUIKit-ObjC
+//
+//  Created by Hansen on 2023/6/4.
+//
+
+#import <YPUIKit/YPUIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YPPickerAlert : YPPopupController
+
+@end
+
+NS_ASSUME_NONNULL_END
